41e25277a36928c4df2899bc3c729ceb060ba559
professorK <jeffpk@gmail.com>
2025-05-14 15:36:13 -0400

    basic input compiling, needs testing

---
+++ kankasaur/FSCli.fs
@@ -0,0 +1,35 @@
+﻿module FSCli
+open System
+open System.CommandLine
+open Microsoft.FSharp.Reflection
+
+module FSCli =
+    let root = RootCommand("Kanka.Net CLI")
+    let rec SubParseDU (duType: Type) (parentCMD: Command) : unit =
+        if FSharpType.IsUnion(duType) then
+            let unionCases = FSharpType.GetUnionCases(duType)
+            for case in unionCases do
+                let name = case.Name
+                let subCommand = Command(name, "tbd")
+                subCommand.SetHandler(fun () -> printfn "Executing %s..." name)
+                parentCMD.Add(subCommand)
+
+                // Process fields of the union case
+                let fields = case.GetFields()
+                for field in fields do
+                    let fieldName = field.Name
+                    let fieldType = field.PropertyType
+                    if FSharpType.IsUnion(fieldType) then
+                        // If the field is a union type, recursively parse it
+                        let subSubCommand = Command(fieldName, "tbd")
+                        subCommand.Add(subSubCommand)
+                        SubParseDU fieldType subSubCommand
+                    else
+                        // Otherwise, add the field as an option
+                        let option = Option<string>(fieldName, description = "tbd")
+                        subCommand.Add(option)
+
+    let ParseDU (duType: Type) : Command =
+        let rootCommand = Command("root", "tbd")
+        SubParseDU duType rootCommand
+        rootCommand
---
+++ kankasaur/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+open System.CommandLine
+open FSCli
+
+type ListCmds =
+    | Campaigns
+    | Maps
+type rootCmds =
+    | List of ListCmds
+    | Create
+    | Update
+    | Delete
+
+[<EntryPoint>]
+let main argv =
+    let rootCommand = FSCli.ParseDU typedefof<rootCmds>
+
+    // Parse and invoke the command line
+    rootCommand.InvokeAsync(argv)
+    0
