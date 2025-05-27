58a2668978050be143d1204261388a3b5f3e70e2
professorK <jeffpk@gmail.com>
2025-05-27 19:28:52 -0400

    Builds. New Foundry Data converter written, not tested or debugged

---
+++ JournalToLoader/Program.fs
@@ -0,0 +1,149 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System.Drawing
+open System.IO
+open System.Text.Json
+
+type MarkerRec = {
+    name : string
+    id : string
+    pageId : string
+    color : Color
+    iconPath : string
+    x : int
+    y : int    
+}
+
+type PageRec = {
+    name : string
+    pageId : string
+    markers : MarkerRec list 
+}
+
+let mutable pages = Map.empty
+
+let DoPage (filePath: string) =
+    let jsonContent = File.ReadAllText filePath
+    let jsonDocument = JsonDocument.Parse(jsonContent)
+    let rootElement = jsonDocument.RootElement
+    let name = rootElement.GetProperty("name").GetString()
+    let pageId = rootElement.GetProperty("_id").GetString()
+    pages <- pages.Add(pageId, { name = name; pageId = pageId; markers = [] })
+    ()
+
+let DoMarkers (filePath: string) =
+    let jsonContent = File.ReadAllText filePath
+    let jsonDocument = JsonDocument.Parse(jsonContent)
+    let rootElement = jsonDocument.RootElement
+    let pageId = rootElement.GetProperty("page_id").GetString()
+    
+    if pages.ContainsKey(pageId) then
+        let name = rootElement.GetProperty("name").GetString()
+        let id = rootElement.GetProperty("_id").GetString()
+        let color = Color.FromArgb(
+            rootElement.GetProperty("color").GetInt32())
+        let iconPath = rootElement.GetProperty("icon").GetString()
+        let x = rootElement.GetProperty("x").GetInt32()
+        let y = rootElement.GetProperty("y").GetInt32()
+
+        let markerRec = {
+            name = name
+            id = id
+            pageId = pageId
+            color = color
+            iconPath = iconPath
+            x = x
+            y = y
+        }
+        pages <- pages.Add(pageId,
+                           { pages.[pageId] with
+                              markers = pages.[pageId].markers @ [markerRec] })
+        ()
+
+        let updatedPage =
+            { pages.[pageId] with markers = markerRec :: pages.[pageId].markers }
+        
+        pages <- pages.Add(pageId, updatedPage)
+    else
+        printfn "Page ID %s not found for markers." pageId
+ 
+type OutputMarker = {
+    name: string
+    color: string
+    txt: string
+    x: int
+    y: int
+}
+type OutputGroup = {
+    name: string
+    markers: OutputMarker list
+}
+type OutputRec = {
+     groups : OutputGroup  list
+}
+
+let GenerateLoaderData (outputFile: string) (pages: Map<string, PageRec>) =
+        // "name":"Seaseyes Tower", "color":"#58ACFA", "x":338, "y":2172,
+        // "txt":"<p>City building</p>", "ref":"$71"
+        use writer = new StreamWriter(outputFile)
+        //make the JSON template
+        let outputData = 
+            pages
+            |> Map.toList
+            |> List.map (fun (_, page) ->
+                {
+                    groups = 
+                        page.markers
+                        |> List.groupBy (fun m -> m.name)
+                        |> List.map (fun (name, markers) ->
+                            {
+                                name = name
+                                markers = 
+                                    markers
+                                    |> List.map (fun m ->
+                                        {
+                                            name = m.name
+                                            color = m.color.ToArgb().ToString("X8")
+                                            txt = sprintf "<p>%s</p>" page.name
+                                            x = m.x
+                                            y = m.y
+                                        })
+                            })
+                })
+        JsonSerializer.Serialize (outputData, 
+            JsonSerializerOptions(PropertyNamingPolicy = JsonNamingPolicy.CamelCase))
+        |> writer.WriteLine
+        
+        
+
+[<EntryPoint>]
+let main argv =
+    
+    if argv.Length <> 2 then
+        printfn "Usage: JournalToLoader <path to journal and scene files> <output file>"
+        1
+    else
+        let filePath = argv.[0]
+        let outputFile = argv.[1]
+        if not (File.Exists filePath) then
+            printfn "File %s does not exist." filePath
+            1
+        else
+            try
+                let jsonFiles= Directory.GetFiles (Path.GetDirectoryName filePath, "*.json")
+                jsonFiles
+                |> Array.filter (fun file ->
+                    file.Contains "JournalEntry")
+                |> Array.iter DoPage
+                 
+                jsonFiles
+                |> Array.filter (fun file ->
+                    file.Contains "Scene")
+                |> Array.iter DoMarkers
+                    
+                GenerateLoaderData outputFile pages
+                0
+            with
+            | ex ->
+                printfn "An error occurred: %s" ex.Message
+                1
---
+++ JsonMarkerLoader/Program.fs
@@ -0,0 +1,82 @@
+﻿// This application reads a JSON file containing map markers and groups
+// and uploads them to Kanka as map markers
+// Note that it has some odd hacks, like reversing of the Y coordinates specific to the
+// size of the Waterdeep map
+// This could be made more generic by fetching the map size from Kanka
+module JBulkLoader
+
+open System.IO
+open System.Text.Json
+open Kanka.NET
+open FSharp.Data
+open kankasaur.MapMarkers
+open kankasaur.MarkerGroup
+
+let mutable offsets = (0, 0)
+
+let DoMarker campaignID mapID groupID (markerInput: JsonElement) =
+    let name = markerInput.GetProperty("name").GetString()
+    let color = markerInput.GetProperty("color").GetString()
+    let x = markerInput.GetProperty("x").GetInt32()
+    let y = (9000-markerInput.GetProperty("y").GetInt32()) 
+    let text = markerInput.GetProperty("txt").GetString()
+    sprintf $" Marker {name} at {x}, {y} with color {color} "
+    
+    NewMarkerJson campaignID mapID groupID name x y 1 1
+    |> CreateMapMarker campaignID mapID 
+    
+
+let MakeGroup campaignID mapID groupName : JsonElement=
+    CreateMarkerGroup campaignID mapID groupName
+         
+   
+
+let GetGroupID campaignID mapID groupName =
+    ListMarkerGroups campaignID mapID
+    |> fun (jel:JsonElement) ->
+        let data = jel.GetProperty("data")
+        data.EnumerateArray()
+        |> Seq.tryFind (fun group -> group.GetProperty("name").GetString() = groupName)
+        |> function
+            | Some group ->
+                group.GetProperty("id").GetInt32().ToString()
+            | None -> 
+                printfn "Group %s not found" groupName
+                CreateMarkerGroup campaignID mapID groupName
+                 |> fun retValue ->
+                    let retGroup = retValue.GetProperty("data")
+                    retGroup.GetProperty("id").GetInt32().ToString()
+
+    
+let DoGroup campaignID mapID (group: JsonElement) =
+    let groupName = group.GetProperty("name").GetString()
+    let groupID = GetGroupID campaignID mapID groupName 
+    group.GetProperty("markers").EnumerateArray()
+    |> Seq.iter (fun marker -> 
+                      DoMarker campaignID mapID groupID marker
+                      |> ignore)
+        
+
+let DoDocument (JsonDocument: JsonDocument) =
+    let docElement = JsonDocument.RootElement
+    let mapID = docElement.GetProperty("mapID").GetString()
+    let campaignID = docElement.GetProperty("campaignID").GetString()
+    let groups = docElement.GetProperty("groups")
+    let docOffsets = docElement.GetProperty("offsets")
+    let offsetX = docOffsets.GetProperty("x").GetInt32()
+    let offsetY = docOffsets.GetProperty("y").GetInt32()
+    offsets <- (offsetX, offsetY)
+    groups.EnumerateArray()
+    |>Seq.iter (fun group -> DoGroup campaignID mapID group)
+    
+
+[<EntryPoint>]
+let main argv =
+    let infile = argv[0]
+    use fileStream = new FileStream(infile, FileMode.Open, FileAccess.Read)
+    use reader = new StreamReader(fileStream)
+    let content = reader.ReadToEnd()
+    let options = JsonDocumentOptions()
+    JsonDocument.Parse(content)
+    |> DoDocument
+    0
